c22443d4cd5a45572330ba6508de698c4de50921
Matthew Somers <matthew.somers.hi@gmail.com>
2013-03-17 22:05:30 -0700

    improvements

--- server-udp.c
+++ server-udp.c
@@ -7,11 +7,12 @@
 
 #define PORT 9930
 
-//1Kbyte, 4KB, 8 KB, 16KB, 32 KB, and 64KB
-#define BUFLEN 1024
+//1 byte, 1KB, 4KB, 8 KB, 16KB, 32 KB, and 64KB
+//#define BUFLEN 1
+//#define BUFLEN 1024
 //#define BUFLEN 4096
 //#define BUFLEN 8192
-//#define BUFLEN 16384
+#define BUFLEN 16384
 //#define BUFLEN 32768
 //#define BUFLEN 65536
 
@@ -40,11 +41,11 @@ int main(void)
 
 
    //main loop, waits, and the sends ack when it receives something
-   while (1) {
-
+   while (1) 
+   {
       recvfrom(s, buf, BUFLEN, 0, &si_other, &slen);
 
-      printf("Received packet from %s:%d\nData: %s\n", 
+      printf("Received packet from %s:%d\nData: %s", 
          inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port), buf);
       
       printf("Sending ack to %s\n\n", inet_ntoa(si_other.sin_addr));
